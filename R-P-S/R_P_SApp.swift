2e39e8db1b415197d2d06e047d65fdbfb609a588
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-06 14:16:27 -0400

    Initial Commit

---
+++ R-P-S/R_P_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_P_SApp.swift
+//  R-P-S
+//
+//  Created by Parth Antala on 2024-07-06.
+//
+
+import SwiftUI
+
+@main
+struct R_P_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
